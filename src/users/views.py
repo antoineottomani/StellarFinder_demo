2a98954b0d11df7dc0eb30e5f429c54d82f80ac5
anto <antoine.ottomani@gmail.com>
2025-02-10 14:32:31 +0100

    update

--- src/users/views.py
+++ src/users/views.py
@@ -1,18 +1,13 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
-from django.contrib.auth import logout
 from .models import CustomUser
-from equipment.models import Telescope, Camera
 from .forms import SignUpForm
 from django.views.generic import CreateView, TemplateView
 from django.shortcuts import redirect
 from django.conf import settings
 import pathlib
 import json
-
-BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
-DEMO_DATA_PATH = BASE_DIR / "equipment/demo_data.json"
 
 
 class SignUpView(CreateView):
@@ -38,7 +33,7 @@ class DemoLoginView(LoginView):
 
 
 def load_demo_data():
-    with open(DEMO_DATA_PATH, "r") as file:
+    with open(settings.DEMO_DATA_PATH, "r") as file:
         return json.load(file)
 
 
@@ -56,28 +51,3 @@ class DemoAccountView(TemplateView):
         # Indicateur du mode démo
         context["is_demo"] = True
         return context
-
-
-class UserAccountView(LoginRequiredMixin, TemplateView):
-    login_url = "/connexion/"
-    model = CustomUser
-    template_name = "users/account.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        # Get equipment of user to render it in account page
-        context["telescopes"] = Telescope.objects.filter(
-            user=self.request.user)
-        context["cameras"] = Camera.objects.filter(user=self.request.user)
-
-        context['ajouter_telescope_action'] = reverse_lazy(
-            'ajouter-telescope')
-        context['ajouter_camera_action'] = reverse_lazy('ajouter-camera')
-
-        return context
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("accueil")
