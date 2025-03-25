83a6c72f9de901f78cdd0193496b3226811d3ecc
anto <antoine.ottomani@gmail.com>
2025-03-25 10:08:22 +0100

    Delete files not needed in demo mode

--- src/equipment/mixins.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-
-
-class RestrictDemoActionsMixin:
-    def dispatch(self, request, *args, **kwargs):
-        # Si on est en mode démo, redirige vers la page démo
-        if request.path.startswith("/demo/"):  # Ou vérifie un autre critère
-            return redirect("compte-demo")
-        return super().dispatch(request, *args, **kwargs)
