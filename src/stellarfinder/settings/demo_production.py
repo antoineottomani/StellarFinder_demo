683c458f7b89247182d29cd184ea6c99cb9b8f87
anto <antoine.ottomani@gmail.com>
2025-04-01 13:53:34 +0200

    change configuration for production

--- src/stellarfinder/settings/demo_production.py
+++ src/stellarfinder/settings/demo_production.py
@@ -3,13 +3,9 @@ from stellarfinder.settings.settings import *
 DEBUG = False
 
 # Define the allowed hosts for the production environment
-ALLOWED_HOSTS = [
-    "stellarfinder.ovh",
-    "www.stellarfinder.ovh",
-    "stellarfinder-env.eba-ywazt47c.eu-west-3.elasticbeanstalk.com",
-]
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", "").split(", ")
 
 # Security settings for production
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
