1469c89a0193f3677c92a21300c659df03c893dc
anto <antoine.ottomani@gmail.com>
2025-04-02 09:31:29 +0200

    changes

--- src/stellarfinder/wsgi.py
+++ src/stellarfinder/wsgi.py
@@ -5,9 +5,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'stellarfinder.settings.demo_production')
 
 # Check if the environment variable is set in production
-# if 'DJANGO_SETTINGS_MODULE' not in os.environ:
-#     raise RuntimeError(
-#         "DJANGO_SETTINGS_MODULE is not set! Make sure to configure it.")
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    raise RuntimeError(
+        "DJANGO_SETTINGS_MODULE is not set! Make sure to configure it.")
 
 
 application = get_wsgi_application()
