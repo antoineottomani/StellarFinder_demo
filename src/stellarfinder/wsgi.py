3ba9f79f3b855d441dac3f4ff6150d1f68396f92
anto <antoine.ottomani@gmail.com>
2025-04-02 09:40:58 +0200

    change path to define module

--- src/stellarfinder/wsgi.py
+++ src/stellarfinder/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'stellarfinder.settings.demo_production')
+                      'settings.demo_production')
 
 # Check if the environment variable is set in production
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
